74a3c08db25d66121442d332573ad221ab7d122f
hyeonmin <junseol86@gmail.com>
2017-04-09 11:21:53 +0900

    EDIT Qna
    everything except deleting question

--- app/controllers/online_consulting/QnaController.scala
+++ app/controllers/online_consulting/QnaController.scala
@@ -15,12 +15,37 @@ class QnaController @Inject()(qnaModel: QnaModel, common: Common) extends Contro
 
   implicit val formats = Serialization.formats(NoTypeHints)
 
+  def getQnas(page: Int, search: String) = Action {
+    var result = Map[String, Any]()
+    result += "data" -> qnaModel.getQnas(page, search)
+    Ok(Json(formats).write(result))
+  }
+
+  def readQuestion() = Action { request =>
+    def fp = new common.FromPost(request)
+    var result = Map[String, Any]()
+    result += "data" -> qnaModel.readQuestion(
+      fp.get("id").toInt, fp.get("selector"), fp.get("validator"),
+      fp.get("password")
+    )
+    Ok(Json(formats).write(result));
+  }
+
   def writeQuestion() = Action { request =>
     def fp = new common.FromPost(request)
     var result = Map[String, Any]()
     result += "data" -> qnaModel.writeQuestion(
       fp.get("asker"), fp.get("email"), fp.get("password"),
       fp.get("title"), fp.get("question")
+    )
+    Ok(Json(formats).write(result));
+  }
+
+  def answerQuestion() = Action { request =>
+    def fp = new common.FromPost(request)
+    var result = Map[String, Any]()
+    result += "data" -> qnaModel.answerQuestion(
+      fp.get("id").toInt, fp.get("answer"), fp.get("selector"), fp.get("validator")
     )
     Ok(Json(formats).write(result));
   }
--- app/models/QnaModel.scala
+++ app/models/QnaModel.scala
@@ -25,7 +25,42 @@ class QnaModel @Inject()(db: Database, common: Common, security: Security, sql: 
          WHERE asker LIKE "%%$searchWord%s%%"
          OR title LIKE "%%$searchWord%s%%"
        """.stripMargin
-    sql.getList("tax_news", condition, page, PAGE_SIZE)
+    sql.getList("qna", condition, page, PAGE_SIZE)
+  }
+
+  def readQuestion(id: Int, selector: String, validator: String, password: String): Map[String, Any] = {
+    if (selector != "" && validator != "") {
+//      관리자일 때
+      val authentication = userModel.authenticate(selector, validator)
+      if (authentication("success") == false) {
+        //      유저확인 실패시 종료
+        return common.returnSuccessResult(false)
+      }
+    } else {
+      //      질문을 올린 사용자일 때
+      val passwordHash = security.sha256Hashing(password)
+      var check = List[Map[String, Any]]()
+      val checkQuery = f"""SELECT password FROM qna WHERE id = $id%d"""
+      db.withConnection { implicit conn =>
+        check = SQL(
+          checkQuery.stripMargin).as(parser.*)
+      }
+      if (check.length == 0 || check(0)("qna.password") != passwordHash) {
+        return common.returnSuccessResult(false)
+      }
+    }
+
+    val readQuery = f"""SELECT id, asker, email, title, question, answer, asked, answered, views FROM qna WHERE id = $id%d"""
+    var read = List[Map[String, Any]]()
+    db.withConnection { implicit conn =>
+      read = SQL(
+        readQuery.stripMargin).as(parser.*)
+    }
+
+    var result = common.returnSuccessResult(true)
+    result += "detail" -> read(0)
+    result
+
   }
 
   def writeQuestion(asker: String, email: String, password: String, title: String, question: String): Map[String, Any] = {
@@ -39,13 +74,47 @@ class QnaModel @Inject()(db: Database, common: Common, security: Security, sql: 
             {asker}, {email}, {password}, {title}, {question}, {asked}
             )
           """
-        ).on('asker -> asker, 'email -> email, 'passeord -> security.sha256Hashing(password), 'title -> title, 'question -> question, 'asked -> common.getDateFromToday(0)).executeInsert()
+        ).on('asker -> asker, 'email -> email, 'password -> security.sha256Hashing(password), 'title -> title, 'question -> question, 'asked -> common.getDateFromToday(0)).executeInsert()
     }
     insertResult match {
       //        에러시 종료
       case Some(i: Long) => return common.returnSuccessResult(true)
       case None => return common.returnSuccessResult(false)
     }
+  }
+
+  def answerQuestion(id: Int, answer: String, selector: String, validator: String): Map[String, Any] = {
+    val authentication = userModel.authenticate(selector, validator)
+    if (authentication("success") == false) {
+      //      유저확인 실패시 종료
+      return common.returnSuccessResult(false)
+    }
+
+    val account: Map[String, String] = authentication("account").asInstanceOf[Map[String, String]]
+    var modifyResult: Int = 0
+
+    db.withConnection { implicit conn =>
+      modifyResult =
+        if (answer != "null")
+          SQL(
+            """UPDATE qna SET
+               answer = {answer}, answerer = {answerer}, answered = {answered}
+               WHERE id = {id}
+            """
+          ).on(
+            'answer -> answer, 'answerer -> account("auth_token.user_id"), 'answered ->  common.getDateFromToday(0), 'id -> id
+          ).executeUpdate()
+      else
+          SQL(
+            """UPDATE qna SET
+               answer = NULL, answerer = {answerer}, answered = {answered}
+               WHERE id = {id}
+            """
+          ).on(
+            'answerer -> account("auth_token.user_id"), 'answered ->  common.getDateFromToday(0), 'id -> id
+          ).executeUpdate()
+    }
+    common.returnSuccessResult(modifyResult == 1)
   }
 
 }
